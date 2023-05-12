3d0a99fbc04019aefbf1a91a6a3f6596e0f21acd
Orçun Çolak <orcun.colak@hazelcast.com>
2023-05-12 12:41:35 +0300

    add from the beginning kafka listener

--- src/main/java/com/colak/kafkatutorial/consumer/FromBeginningKafkaMessageConsumer.java
+++ src/main/java/com/colak/kafkatutorial/consumer/FromBeginningKafkaMessageConsumer.java
@@ -3,20 +3,25 @@ package com.colak.kafkatutorial.consumer;
 import com.colak.kafkatutorial.producer.KafkaTopicConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 
 @Component
 @Slf4j
-public class KafkaMessageConsumer {
-    /*
-    @KafkaListener(topics = KafkaTopicConfig.TOPIC_NAME, groupId = "my-group-id5")
-    public void listen(@Payload(required = false) String value, @Headers Map<String, Object> headers) {
-        String key = (String) headers.get("kafka_receivedMessageKey");
-        log.info("Received key : {} value : {}", key, value);
-        // Process the message as per your requirement
-    }*/
+public class FromBeginningKafkaMessageConsumer implements ConsumerSeekAware {
+
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        // Seek to the beginning of each assigned partition
+        assignments.keySet().forEach(
+                topicPartition -> callback.seekToBeginning(topicPartition.topic(), topicPartition.partition()));
+
+    }
 
     @KafkaListener(topics = KafkaTopicConfig.TOPIC_NAME)
     public void listen(ConsumerRecord<String, String> record) {
